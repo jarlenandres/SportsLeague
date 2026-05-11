a57160b159739489be89125ac441bd06cf8a5c83
jarlenandres <jaarojas@gmai.com>
2026-05-11 08:32:45 -0500

    creation of the SponsorRepository in the DataAccess

---
+++ SportsLeague.DataAccess/Repositories/SponsorRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsLeague.DataAccess.Context;
+using SportsLeague.Domain.Entities;
+using SportsLeague.Domain.Enums;
+using SportsLeague.Domain.Interfaces.Repositories;
+
+namespace SportsLeague.DataAccess.Repositories;
+
+public class SponsorRepository : GenericRepository<Sponsor>, ISponsorRepository
+{
+    public SponsorRepository(LeagueDbContext context) : base(context)
+    {
+    }
+
+    public async Task<Sponsor?> GetByNameAsync(string name)
+    {
+        return await _dbSet
+            .Include(s => s.TournamentSponsors)
+                .ThenInclude(ts => ts.Tournament)
+            .FirstOrDefaultAsync(s => s.Name == name);
+    }
+
+    public async Task<IEnumerable<Tournament>> GetBySponsorsAsync(SponsorCategory category)
+    {
+        return await _dbSet
+            .Where(s => s.Category == category)
+            .SelectMany(s => s.TournamentSponsors.Select(ts => ts.Tournament))
+            .ToListAsync();
+    }
+}
