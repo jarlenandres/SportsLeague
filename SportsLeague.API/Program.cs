f60d0ea1749ce1a70963637b45c01bfc6f75a34b
jarlenandres <jaarojas@gmai.com>
2026-05-11 13:33:02 -0500

    code update in the program.cs

--- SportsLeague.API/Program.cs
+++ SportsLeague.API/Program.cs
@@ -26,6 +26,7 @@ builder.Services.AddScoped<ITeamService, TeamService>();
 builder.Services.AddScoped<IPlayerService, PlayerService>();
 builder.Services.AddScoped<IRefereeService, RefereeService>();
 builder.Services.AddScoped<ITournamentService, TournamentService>();
+builder.Services.AddScoped<ISponsorService, SponsorService>();
 
 // -- AutoMapper --
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
